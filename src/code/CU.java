5df8dda3444a1775193810b49960551abb9455cf
topherCantrell <topherCantrell@gmail.com>
2015-06-18 10:19:09 -0500

    Building up java code.

---
+++ src/code/CU.java
@@ -0,0 +1,33 @@
+package code;
+
+public class CU {
+	
+	/**
+	 * This function parses an integer specification. The string may start with "0x" (hex)
+	 * or "0b" (binary) or "0d" (decimal). All underscores are stripped out.
+	 * @param s the string to parse
+	 * @return the integer representation
+	 */
+	public static int parseInt(String s, int base) {
+		if(s.startsWith("0x")) {
+			s = s.substring(2);
+			base = 16;
+		} else if(s.startsWith("0b")) {
+			s = s.substring(2);
+			base = 2;
+		}
+		s = s.replace("_","");
+		// Throws an exception if not valid
+		return Integer.parseInt(s, base);		
+	}
+	
+	public static boolean isNumeric(String s, int base) {
+		try {
+			parseInt(s,base);
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
+	}
+
+}
--- src/code/CodeLine.java
+++ src/code/CodeLine.java
@@ -1,20 +1,82 @@
 package code;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
 public class CodeLine {
 	
-	private String originalText;
-	private String filename;
-	private int line;
+	/**
+	 * This function loads all the CodeLines from a given file.
+	 * @param filename the file to parse
+	 * @return the list of CodeLines
+	 * @throws IOException
+	 */
+	public static List<CodeLine> loadLines(Path filename) throws IOException {
+		List<CodeLine> ret = new ArrayList<CodeLine>();		
+		List<String> lines = Files.readAllLines(filename);
+		
+		int n = 1;
+		for(String s : lines) {
+			CodeLine c = new CodeLine(s,filename.toString(), n);
+			ret.add(c);
+		}
+		
+		return ret;		
+	}
 	
-	private int address = -1;
-	private byte[] data = null;
-	private String opcode = null;
-	private int opcodePos = -1;	
-	private String comment = null;
-	private int commentPos = -1;
+	/**
+	 * This function does a simple checksum over all "data" in the
+	 * code lines.
+	 * @param code the list of code lines
+	 * @return the simple checksum
+	 */
+	public static long simpleDataChecksum(List<CodeLine> code) {
+		long ret = 0;
+		for(CodeLine line : code) {
+			if(line.data!=null) {
+				for(int i : line.data) {
+					ret = ret + i;
+				}
+			}
+		}		
+		return ret;
+	}
 	
-	private String label = null;
-	private String[] collectedLabels = null;
+	/**
+	 * This function does a simple checksum over the bytes in a file.
+	 * @param file path to the data file
+	 * @return the simple checksum
+	 * @throws IOException
+	 */
+	public static long simpleBinaryFileChecksum(Path file) throws IOException {
+		long ret = 0;		
+		InputStream is = new FileInputStream(file.toString());
+		while(is.available()>0) {
+			ret = ret + is.read();
+		}
+		is.close();
+		return ret;
+	}
+	
+	String originalText;
+	String filename;
+	int line;
+	
+	int address = -1;
+	List<Integer> data = null;
+	String opcode = null;
+	int opcodePos = -1;	
+	String comment = null;
+	int commentPos = -1;
+	
+	String label = null;
+	String[] collectedLabels = null;
 	
 	// PrintChar:
 	// 4000: 01 02 03  LDA  $500   ; This is a comment
@@ -23,15 +85,88 @@ public class CodeLine {
 	// PrintChar:
 	//    STA tmp1 ; This is a comment
 	
+	/**
+	 * This creates a new CodeLine object from the given line of text.
+	 * @param originalText the line of text (usually from a text file)
+	 * @param filename the name of the file (for error reporting)
+	 * @param line the line number (for error reporting)
+	 */
 	public CodeLine(String originalText, String filename, int line) {
+		
 		this.originalText = originalText;
 		this.filename = filename;
-		this.line = line;		
-	}
-	
-	public static void main(String [] args) {
+		this.line = line;
 		
-		System.out.println("Still here");
+		// comment		
+		commentPos = originalText.indexOf(";");
+		if(commentPos>=0) {
+			comment = originalText.substring(commentPos+1);
+			originalText = originalText.substring(0,commentPos);
+		}
+		
+		int pos = 0; // Where we are moving across the string		
+		
+		// label
+		String [] words = originalText.trim().split(" ");
+		if(words.length>0 && words[0].endsWith(":")) {
+			String lab = words[0].substring(0,words[0].length()-1);
+			int i = originalText.indexOf(":");
+			pos = i+1;
+			if(CU.isNumeric(lab,16)) {
+				this.address = CU.parseInt(lab, 16);
+			} else {
+				this.label = lab;
+			}
+		}
+		
+		// data
+		List<Integer> dat = new ArrayList<Integer>();
+		while(pos<originalText.length()) {
+			if(originalText.charAt(pos)==' ') {
+				pos = pos + 1;
+				continue;
+			}
+			if(pos > originalText.length()-2) { // Can't be valid
+				break;
+			}
+			if(pos == originalText.length()-2) { // Right on the end
+				if(CU.isNumeric(originalText.substring(pos,pos+2), 16)) {
+					dat.add(CU.parseInt(originalText.substring(pos,pos+2), 16));
+					pos = pos + 2;
+					break;
+				}
+			}
+			if(originalText.charAt(pos+2)!=' ') { // Not a two digit value
+				break;
+			}
+			if(CU.isNumeric(originalText.substring(pos,pos+2), 16)) {
+				dat.add(CU.parseInt(originalText.substring(pos,pos+2), 16));
+				pos = pos + 2;
+			}
+		}
+		if(dat.size()>0) {
+			this.data = dat;
+		}
+		
+		// opcode
+		this.opcode = originalText.substring(pos).trim();
+		if(this.opcode.length()>0) {
+			this.opcodePos = pos;
+			System.out.println(this.opcode);
+		} else {
+			this.opcode = null;
+		}		
+		
+	}	
+	
+	public static void main(String [] args) throws Exception {
+		
+		Path p = Paths.get("content/coco/madnessminotaur/Code.mark");
+		Path pd = Paths.get("content/coco/madnessminotaur/madness.bin");
+		List<CodeLine> lines = CodeLine.loadLines(p);
+		
+		System.out.println(CodeLine.simpleDataChecksum(lines));
+		System.out.println(CodeLine.simpleBinaryFileChecksum(pd));
 		
 	}
 
