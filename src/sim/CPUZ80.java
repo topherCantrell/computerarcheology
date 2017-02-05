1be52a8464eb634af8ed1fb77dcff79aad90de76
tophe <tophe@DESKTOP-V5URPEM>
2017-02-04 18:05:16 -0600

    lots of work

--- src/sim/CPUZ80.java
+++ src/sim/CPUZ80.java
@@ -9,15 +9,22 @@ public class CPUZ80 extends CPU {
 	int [] memory = new int[64*1024];
 	
 	int pc;
+	
+	boolean zf, cf, sf;
 
 	public CPUZ80(List<String> lines) {
 		super(lines);
 		
 		registers.put("SP", new Register("SP",true));
 		registers.put("A", new Register("A",false));
-		registers.put("B", new Register("A",false));
+		registers.put("B", new Register("B",false));
+		registers.put("C", new Register("C",false));
+		registers.put("D", new Register("D",false));
+		registers.put("E", new Register("E",false));
 		registers.put("H", new Register("H",false));
 		registers.put("L", new Register("L",false));
+		registers.put("BC", new RegisterPair("BC",getRegister("B"),getRegister("C")));
+		registers.put("DE", new RegisterPair("DE",getRegister("D"),getRegister("E")));
 		registers.put("HL", new RegisterPair("HL",getRegister("H"),getRegister("L")));
 		
 		for(String s : lines) {			
@@ -76,10 +83,19 @@ public class CPUZ80 extends CPU {
 	@Override
 	public int pop(boolean word) {
 		// read MSB
-		// SP = SP - 1
+		// SP = SP + 1
 		// read LSB
-		// SP = SP - 1
-		throw new RuntimeException("Implement me");
+		// SP = SP + 1
+		Accessable sp = getRegister("SP");
+		int spval = sp.readValue();
+		int rval = memory[spval];
+		spval = spval + 1;
+		if(word) {
+			rval = (rval<<8) | memory[spval];
+			spval = spval + 1;
+		}
+		sp.writeValue(spval);
+		return rval;
 	}
 	
 	public void debug(String addr, String instruction) {
@@ -111,7 +127,9 @@ public class CPUZ80 extends CPU {
 			if(
 					flow(op,par) ||
 					load(op,par) ||
-					math(op,par)
+					math(op,par) ||
+					stack(op,par) ||
+					misc(op,par)
 				) 
 			{
 				debug(addr,instruction);
@@ -139,18 +157,76 @@ public class CPUZ80 extends CPU {
 		return new IndexAccess(this,r,isWordAccess);		
 	}
 	
-	public void setFlags(int value, int C, int Z, int P, int S, int N, int H) {
-		// -1 means calculate from value		
+	boolean intToBool(int v) {
+		if(v==0) return false;
+		return true;
+	}
+	public void setFlags(int value, boolean isWordAccess,
+			boolean C, int Z, int P, int S, int N, int H) 
+	{
+		// -1 means calculate from value
+		cf=C;
+		if(Z>=0) zf=intToBool(Z);
+		else zf=(value==0);
+		if(S>=0) sf=intToBool(S);
+		else sf = (value&128)>0;		
 	}
 	
 	public boolean math(String op, String par) {
+		if(op.equals("INC")) {
+			Accessable ac = getRegister(par);
+			int val = ac.readValue();
+			val = val + 1;
+			ac.writeValue(val);
+			setFlags(val, false, cf, -1, -1, -1, 0, 1);
+			return true;
+		}
 		if(op.equals("AND")) {
 			int val = getRegister(par).readValue();
 			int aval = getRegister("A").readValue();
 			aval = aval & val;
 			getRegister("A").writeValue(aval);			
-			//             C   Z   P   S  N  H
-			setFlags(aval, 0, -1, -1, -1, 0, 1); // Set Z,S,P   C=0,N=0,H=1
+			//                    C       Z   P   S  N  H
+			setFlags(aval, false, false, -1, -1, -1, 0, 1); // Set Z,S,P   C=0,N=0,H=1
+			return true;
+		}
+		if(op.equals("CP")) {
+			int aval = getRegister("A").readValue();
+			if(par.startsWith("$")) {
+				Accessable cv = new AccessConstant(Integer.parseInt(par.substring(1),16),false);
+				aval = aval - cv.readValue();
+				boolean cf = aval<0;
+				if(cf) aval=aval+256;
+				setFlags(aval, false, cf, -1, -1, -1, 0, 1);
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public boolean misc(String op, String par) {
+		if(op.equals("EX")) {
+			String [] parts = par.split(",");
+			Accessable left = getRegister(parts[0]);
+			Accessable right = getRegister(parts[1]);
+			int lv = left.readValue();
+			left.writeValue(right.readValue());
+			right.writeValue(lv);
+			return true;
+		}
+		return false;
+	}
+	
+	public boolean stack(String op, String par) {
+		if(op.equals("PUSH")) {
+			Accessable reg = getRegister(par);
+			push(reg.readValue(),reg.isWordSize());
+			return true;
+		}
+		if(op.equals("POP")) {
+			Accessable reg = getRegister(par);
+			reg.writeValue(pop(reg.isWordSize()));
+			return true;
 		}
 		return false;
 	}
@@ -182,11 +258,25 @@ public class CPUZ80 extends CPU {
 		return false;
 	}
 	
+	public boolean evaluateCondition(String cond) {
+		if(cond.equals("NZ")) return !zf;
+		if(cond.equals("Z")) return zf;
+		throw new RuntimeException("Unknown condition "+cond);
+	}
+	
 	public boolean flow(String op, String par) {
 		if(op.equals("JP")) {
+			boolean take = true;
+			int i = par.indexOf(",");
+			if(i>=0) {
+				take = evaluateCondition(par.substring(0, i));
+				par = par.substring(i+1);
+			}
 			if(par.startsWith("$")) {
 				int dest = Integer.parseInt(par.substring(1),16);
-				pc = findCodeTarget(dest);
+				if(take) {
+					pc = findCodeTarget(dest);
+				}
 				return true;
 			}
 		}
@@ -197,17 +287,33 @@ public class CPUZ80 extends CPU {
 				ROMCALL_DISPLAY();
 				return true;
 			}
+			if(dest==0x2B) {
+				ROMCALL_GETCHAR();
+				return true;
+			}
 			push(pc,true);			
 			pc = findCodeTarget(dest);
 			return true;
 		}
 		
+		if(op.equals("RET")) {
+			boolean take = true;
+			if(par!=null) take = evaluateCondition(par);
+			if(take) {
+				pc = pop(true);
+			}
+			return true;
+		}		
 		
 		return false;
 	}
 	
 	void ROMCALL_DISPLAY() {
-		System.out.println("DISPLAY:"+getRegister("A").readValue());
+		System.out.println("DISPLAY:"+(char)getRegister("A").readValue());
+	}
+	
+	void ROMCALL_GETCHAR() {
+		getRegister("A").writeValue(65);
 	}
 
 }
