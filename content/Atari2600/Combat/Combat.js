9a01e31016fe6f59fd723b15fd53c12a359d843e
ccantrell <ccantrell@HSV-CCANTRELL.INT.vertivco.com>
2017-11-28 12:43:07 -0600

    Finished the playfields

--- content/Atari2600/Combat/Combat.js
+++ content/Atari2600/Combat/Combat.js
@@ -2,24 +2,36 @@ var Combat = (function() {
 	
 	var my = {};
 	
-	my.getPlayfield = function(index) {
+	my.getPlayfield = function(index, address) {
 		
-		//var addrPF0, addrPF1, addrPF2;
-		//if(index==0) {
-			addrPF0 = 0xF775;
-			addrPF1 = 0xF781;
-			addrPF2 = 0xF78D;
-		//}
+		var addrPF0, addrPF1, addrPF2;
+		if(index==0) {
+			addrPF0 = 0xF779;
+			addrPF1 = 0xF785;
+			addrPF2 = 0xF791;
+		} else if(index==1) {
+			addrPF0 = 0xF779; 
+			addrPF1 = 0xF79D;
+			addrPF2 = 0xF79D;
+		} else if(index==2) {
+			addrPF0 = 0xF779; 
+			addrPF1 = 0xF7AE;
+			addrPF2 = 0xF7BA;
+		} else {
+			addrPF0 = 0xF79E; 
+			addrPF1 = 0xF7A1;
+			addrPF2 = 0xF7A1;
+		}
 		
 		// Return 20x16
 		
 		var ret = [];
 		
-		var data0 = BinaryData.getData(addrPF0,16);
-		var data1 = BinaryData.getData(addrPF1,16);
-		var data2 = BinaryData.getData(addrPF2,16);
+		var data0 = BinaryData.getData(addrPF0,12);
+		var data1 = BinaryData.getData(addrPF1,12);
+		var data2 = BinaryData.getData(addrPF2,12);
 		
-		for (var x=0;x<16;++x) {
+		for (var x=0;x<12;++x) {
 			var a = data0[x].toString(2);
     		while(a.length<8) a="0"+a;
     		var b = data1[x].toString(2);
@@ -27,14 +39,14 @@ var Combat = (function() {
     		var c = data2[x].toString(2);
     		while(c.length<8) c="0"+c;
     		
-    		for(var y=4;y<8;++y) {    			
-    			// Only 4 bits in PF0
+    		for(var y=3;y>=0;--y) {    			
+    			// Only upper 4 bits in PF0
     			ret.push(parseInt(a.charAt(y)));    			
     		}    		
     		for(y=0;y<8;++y) {
     			ret.push(parseInt(b.charAt(y)));
     		}
-    		for(y=0;y<8;++y) {
+    		for(y=7;y>=0;--y) {
     			ret.push(parseInt(c.charAt(y)));
     		}
 			
