5ee14beb2d690fda5290006b669469a2cf66665c
Christopher Cantrell <topherCantrell@gmail.com>
2019-04-10 15:27:30 -0500

    Monitor work

--- pysrc/app_asm.py
+++ pysrc/app_asm.py
@@ -2,11 +2,11 @@ from assembler import asm
 
 if __name__ == '__main__':
 
-    asa = asm.Assembler('../../RC2014/code/echo.asm')
+    asa = asm.Assembler('../../RC2014/code/monitor.asm')
 
     try:
         asa.assemble()
-        asa.write_listing('../../RC2014/code/echo.lst')
-        asa.write_binary('../../RC2014/code/echo.bin')
+        asa.write_listing('../../RC2014/code/monitor.lst')
+        asa.write_binary('../../RC2014/code/monitor.bin')
     except asm.ASMException as ex:
         print(ex, ex.line)
