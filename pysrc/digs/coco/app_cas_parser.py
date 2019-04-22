8413a1c0dcf7bf0af20ca86b11082d3a2e24edb5
Christopher Cantrell <topherCantrell@gmail.com>
2019-04-22 15:56:35 -0500

    refactor

--- pysrc/digs/coco/app_cas_parser.py
+++ pysrc/digs/coco/app_cas_parser.py
@@ -1,17 +1,21 @@
-from digs.coco.app_make_madness_bin import pos
-
 
 with open('../../../content/coco/madnessminotaur/s_one.cas', 'rb') as f:
     data = f.read()
 
+
+def skip_leader(data, pos):
+    while data[pos] != 0x55:
+        pos += 1
+    while data[pos] != 0x3C:
+        pos += 1
+    return pos
+
+
 pos = 0
 
-while data[pos] != 0x55:
-    pos += 1
-while data[pos] != 0x3C:
-    pos += 1
+pos = skip_leader(data, pos)
 
-bt = data[pos:pos + 1]
+bt = data[pos]
 pos += 1
 
 print(bt, pos)
