90be2d8f0b464054b6b02e5ff57b3466558070c7
Chris Cantrell <topherCantrell@gmail.com>
2020-10-10 09:59:43 -0500

    Moved opcodetools to separate repo

--- opcodetools/tests/test_cpu/test6809.asm
+++
@@ -1,9 +0,0 @@
-
-._CPU = 6809
-._default_base_page = true
-
-0x0000:
-
-    LDA   <0x50  ; Force base page
-    LDA   >0x50  ; Force direct
-    LDA   0x50   ; Let the assembler choose
--- opcodetools/tests/test_cpu/test8052.asm
+++
@@ -1,8 +0,0 @@
-
-._CPU = 8052
-
-0x0000:
-
-    JMP    @A+DPTR
-    MOV    A , #10     ; Put 10 in the accumulator
-
